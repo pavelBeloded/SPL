9702f80adff13eb738be0676803d168fbaed5575
PavelBeloded <beloded_2007@bk.ru>
2025-09-04 13:17:32 +0300

    lab3

--- lab_3/script.js
+++ lab_3/script.js
@@ -27,11 +27,50 @@ const students = [
   { name: "Ольга", age: 17, groupId: 101 },
   { name: "Дмитрий", age: 21, groupId: 104 },
   { name: "Светлана", age: 19, groupId: 102 },
-  { name: "Сергей", age: 22, groupId: 104 }
+  { name: "Сергей", age: 22, groupId: 104 },
 ];
 
 function filterStudents(students) {
-    return students.filter((student) => student.age > 17)
+  return students.filter((student) => student.age > 17);
 }
 
-console.log(filterStudents(students))
+console.log(filterStudents(students));
+
+// #4 --------------------------
+
+function convertString(string) {
+  const stringArr = string.split("");
+  let codes = stringArr.map((item) => "" + item.charCodeAt());
+  let total1 = Number(codes.join(""));
+
+  let convertedCodes = codes.map((item) => item.replaceAll("7", "1"));
+  let total2 = Number(convertedCodes.join(""));
+  return total1 - total2;
+}
+
+// #5 -----------------------
+
+function extend(...objects) {
+  return JSON.parse(JSON.stringify(Object.assign(...objects)));
+}
+
+let obj = { a: 1, b: 2 };
+
+console.log(extend(obj, { c: 3 }) === obj);
+
+// #6 --------------------------
+
+function printPyramid(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let line = "";
+
+    for (let j = 1; j <= rows - i; j++) {
+      line += " ";
+    }
+
+    for (let k = 1; k <= 2 * i - 1; k++) {
+      line += "*";
+    }
+    console.log(line);
+  }
+}
